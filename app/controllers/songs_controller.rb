411b0e35234100bed8aa556798f6cecce254dd42
christianalim <christianalim1@gmail.com>
2019-07-22 22:27:23 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,15 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    find_song
+    @songs = Song.all
   end
 
   def new
+    @song = Song.new
   end
 
   def create
@@ -42,6 +46,10 @@ class SongsController < ApplicationController
   end
 
   private
+
+  def find_song
+    @song = Song.find(params[:id])
+  end
 
   def song_params
     params.require(:song).permit(:title)
